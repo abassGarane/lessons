18ad517bb80f5e975d411c14631c7f3d30f53897
abassGarane <thelibertinewreck@gmail.com>
2022-09-23 15:56:34 +0300

    Corrections on grpc.student.request

--- grpc/servers/student.go
+++ grpc/servers/student.go
@@ -17,11 +17,12 @@ func NewStudentServer(l hclog.Logger) StudentServer {
 }
 func (s StudentServer) RegisterStudent(ctx context.Context, srq *protos.StudentRequest) (*protos.Response, error) {
 	s.log.Info("Handle RegisterStudent:: name", srq.GetName(), "Student AdmissionNUmber", srq.GetAdmNumber())
+	age := uint32(2020 - srq.GetYob())
 	student := &protos.StudentResponse{
 		Form:      srq.GetForm(),
 		Name:      srq.GetName(),
-		Yob:       srq.GetYob(),
 		AdmNumber: srq.GetAdmNumber(),
+		Age:       age,
 	}
 	studentsStore[srq.AdmNumber] = student
 	return &protos.Response{}, nil
